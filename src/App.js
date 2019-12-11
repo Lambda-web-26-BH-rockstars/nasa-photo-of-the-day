142b9b3f50c2ee6af423e86f29ff031b70ee15c0
Brian Hague <alphaseinor@gmail.com>
2019-12-11 12:32:56 -0600

    added image from axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, {useEffect} from "react";
-import "./App.css";
+import React, {useEffect, useState} from "react"
+import axios from 'axios'
+import "./App.css"
 
 function App() {
 
+  const [photoOfTheDay, setPhotoOfTheDay] = useState('')
 
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=EwTD8nntFpQflSE62RAmu8EfYamxWLU95x1Gau7D&date=2019-12-10')
+      .then(res =>{
+        console.log(res.data)
+        setPhotoOfTheDay(res.data)
+      })
+      .catch(err =>{
+        console.log(err)
+      })
+  },[])
 
   return (
     <div className="App">
       <div className="photoHolder">
-        this is a div
+        <img src={photoOfTheDay.url} alt={photoOfTheDay.title} />
       </div>
     </div>
   );
